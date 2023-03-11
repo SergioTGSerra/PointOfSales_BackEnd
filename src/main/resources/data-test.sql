9d3f73244b3e768802e42e3c6f6d1877154e985f
Serra <sergioserra.2003@gmail.com>
2023-03-11 21:40:59 +0000

    Add Data data-test.sql

--- src/main/resources/data-test.sql
+++ src/main/resources/data-test.sql
@@ -11,5 +11,17 @@ INSERT INTO order_status (name) VALUES
                                     ('Finalizado'),
                                     ('Cancelado');
 
+INSERT INTO categories (id, name, description, is_active) VALUES
+                                    (1, 'Sandes', 'Sandes', true),
+                                    (2, 'Pratos', 'Pratos', true),
+                                    (3, 'Pizzas', 'Pizzas', true),
+                                    (4, 'Bebidas', 'Bebidas', true);
+
+INSERT INTO public.tax (id, is_deleted, name, tax)
+VALUES (1, false, '1', 1);
+
+INSERT INTO public.products (id, is_active, is_deleted, name, price, stock, category_id, tax_id)
+VALUES (1, true, false, '1', 1, 1, 1, 1);
+
 /*INSERT INTO entities (name, is_active, is_deleted, id_entity_type) VALUES
     ('Utilizador não autenticado', 1, 0, 1);*/
