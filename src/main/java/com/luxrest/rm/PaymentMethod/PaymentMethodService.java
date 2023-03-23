b8d018b5828a90fdd63707754e526c00c05c39c1
Serra <sergioserra.2003@gmail.com>
2023-03-23 21:20:54 +0000

    Clean Code And Resolve Bugs PaymentMethodService

--- src/main/java/com/luxrest/rm/PaymentMethod/PaymentMethodService.java
+++ src/main/java/com/luxrest/rm/PaymentMethod/PaymentMethodService.java
@@ -1,18 +1,15 @@
 package com.luxrest.rm.PaymentMethod;
 
-import com.luxrest.rm.Tax.Tax;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class PaymentMethodService {
     private final PaymentMethodRepository paymentMethodRepository;
-
-    public PaymentMethodService(PaymentMethodRepository paymentMethodRepository) {
-        this.paymentMethodRepository = paymentMethodRepository;
-    }
 
     public List<PaymentMethod> getAllPaymentMethod(){
         return paymentMethodRepository.findAll();
@@ -30,12 +27,10 @@ public class PaymentMethodService {
     }
 
     public PaymentMethod updatePaymentMethod(Integer id, PaymentMethod paymentMethod){
-        PaymentMethod existingPaymentMethod = paymentMethodRepository.findById(id)
+        paymentMethodRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Payment Method not found"));
-        existingPaymentMethod.setName(paymentMethod.getName());
-        existingPaymentMethod.setDescription(paymentMethod.getDescription());
-        existingPaymentMethod.setIsActive(paymentMethod.getIsActive());
-        return paymentMethodRepository.save(existingPaymentMethod);
+        paymentMethod.setId(id);
+        return paymentMethodRepository.save(paymentMethod);
     }
 
     public PaymentMethod deletePaymentMethod(Integer id){
