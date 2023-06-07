fe35c98c006cdb0422308a2fb8a4e486131adc37
serra <sergioserra.2003@gmai.com>
2023-06-07 17:40:20 +0100

    Promotion Update

--- src/main/java/com/luxrest/rm/Promotion/Promotion.java
+++ src/main/java/com/luxrest/rm/Promotion/Promotion.java
@@ -37,7 +37,13 @@ public class Promotion {
     private Date createdAt;
 
     @PrePersist
-    private void onCreate() {
+    public void prePersist() {
         createdAt = new Date();
+        if (this.isDeleted == null) {
+            this.isDeleted = false;
+        }
+        if(this.isActive == null){
+            this.isActive = true;
+        }
     }
 }
